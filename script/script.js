f7a958c389880f036c41bcca37d9c9f8e453173b
Shishov Anatolii <i-testic@yandex.ru>
2022-03-13 14:02:59 +0300

    feat: редактировать проект после второго ревью (3-st edition)

--- script/script.js
+++ script/script.js
@@ -1,18 +1,17 @@
 let popup = document.querySelector('.popup');
-let openPopup = document.querySelector('.profile__button_change_edit');
+let openPopup = document.querySelector('.profile__button_type_edit');
 let closePopup = popup.querySelector('.popup__button-close');
 let profile = document.querySelector('.profile');
 let nameProfile = profile.querySelector('.profile__name');
 let hobbyProfile = profile.querySelector('.profile__hobby');
-let popupFieldName = popup.querySelector('.popup__field_name_edit');
-let popupFieldHobby = popup.querySelector('.popup__field_hobby_edit');
+let popupFieldName = popup.querySelector('.popup__field_type_name');
+let popupFieldHobby = popup.querySelector('.popup__field_type_hobby');
 let popupForm = popup.querySelector('.popup__form');
-
-popupFieldName.value = nameProfile.textContent;
-popupFieldHobby.value = hobbyProfile.textContent;
 
 function togglePopup() {
   popup.classList.toggle('popup_opened');
+  popupFieldName.value = nameProfile.textContent;
+  popupFieldHobby.value = hobbyProfile.textContent;
 }
 
 function submitForm(event) {
